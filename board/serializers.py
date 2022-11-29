9a44f287a872efba777ef25334e47e2d53b085fc
Mingyeongdo <mgd813@gmail.com>
2022-11-29 14:22:33 +0900

    feat : blog 조회수 추가 및 View 리팩토링

--- board/serializers.py
+++ board/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 class BlogSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source = 'user.email')
+    views = serializers.IntegerField(read_only=True)
     class Meta:
         model = Blog
-        fields = ('id','title','created_at','updated_at','user','body')
+        fields = ('id','title','created_at','updated_at','user','body','views')
