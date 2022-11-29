f7107d431960d46679652b63f7d62aa3d1f1eea3
Mingyeongdo <mgd813@gmail.com>
2022-11-29 16:25:40 +0900

    feat : 게시판 필터링 기능, 통계페이지, 페이징 설정

--- board/views.py
+++ board/views.py
@@ -1,12 +1,14 @@
 from .models import Blog
-from .serializers import BlogSerializer
+from .serializers import *
 from rest_framework import viewsets
 from .permissions import *
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import *
 from rest_framework.generics import *
 from rest_framework.response import Response
-
+from rest_framework import status
 from django.shortcuts import render
+from rest_framework.views import APIView
+import django_filters
 
 import logging
 logger = logging.getLogger('my')
@@ -27,7 +29,9 @@ class BlogView(ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
+    # 필터 기능 추가
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['user']
     def perform_create(self, serializer):
         # 현재 요청한 유저를 작성자로 설정
         serializer.save(user=self.request.user)
@@ -44,3 +48,17 @@ class BlogDetailView(RetrieveUpdateDestroyAPIView):
         instance.save()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+
+class BlogStatisticsView(APIView):
+
+    permission_classes = [AllowAny]
+    def get(self, request):
+        male_cnt = Blog.objects.filter(user__gender="M").count()
+        female_cnt = Blog.objects.filter(user__gender="F").count()
+
+        return Response({"Male":male_cnt, "Female": female_cnt}, status=status.HTTP_200_OK)
+
+    # def post(self, request):
+    #     user = request.GET.get('user')
+    #     user_board = Blog.objects.filter(user__email=user).count()
+    #     return Response({f"{user} 의 게시물 개수": user_board}, SAFE_METHODS=status.HTTP_200_OK)
