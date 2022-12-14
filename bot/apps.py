8af77a3a9cd937ff5b6685778c7801c3df45c33b
Mingyeongdo <mgd813@gmail.com>
2022-12-14 19:35:24 +0900

    fix : apscheduler 비활성화

--- bot/apps.py
+++ bot/apps.py
@@ -1,10 +1,10 @@
-from django.apps import AppConfig
-from django.conf import settings
+# from django.apps import AppConfig
+# from django.conf import settings
 
-class botConfig(AppConfig):
-    name = 'bot'
+# class botConfig(AppConfig):
+#     name = 'bot'
 
-    def ready(self):
-        if settings.SCHEDULER_DEFAULT:
-            from . import scheduler
-            scheduler.start()
+#     def ready(self):
+#         if settings.SCHEDULER_DEFAULT:
+#             from . import scheduler
+#             scheduler.start()
