0229146f4d1e8c0070564b2f4b315d53b630ff76
Mingyeongdo <mgd813@gmail.com>
2022-12-16 11:57:37 +0900

    chore: 필요없는 requirements 삭제 및 boto3 파일 수정

--- myproject/s3upload.py
+++ myproject/s3upload.py
@@ -1,13 +1,26 @@
 import boto3
 from pathlib import Path
 import gzip    
+import environ
+import os
+
+BASE_DIR=Path.home()/'etl'/'ETL_pipeline'
+
+
+env = environ.Env(DEBUG=(bool, True))
+
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+
 
 def compressor():
     #압축할 로그파일 경로 생성
-    logfile_path = Path.cwd()/'logs'/'encrypted_log.json'
+    logfile_path = BASE_DIR/'logs'/'encrypted_log.json'
     logfile_path = logfile_path.resolve() #절대경로 반환
 
-    compressfile_path = Path.cwd()/'logs'/'compressed_log.gz'
+    compressfile_path = BASE_DIR/'logs'/'compressed_log.gz'
 
     with open(logfile_path,'rb') as f_in:
         with gzip.open(compressfile_path,'wb') as f_out:
@@ -18,12 +31,18 @@ def s3_upload():
     compressor() #encrypted_logs파일을 통해 압축된 로그 파일 생성하고 시작
 
     #s3 client 생성
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3',
+            aws_access_key_id=env('AWS_KEY_ID'),
+            aws_secret_access_key=env('AWS_ACCESS_KEY'))
 
-    currentpath = str(Path.cwd()) #/Users/orangehour/projects/ETL_pipeline
+    currentpath = str(BASE_DIR) #/Users/gyeongdo/projects/ETL_pipeline
     bucket_name = 'team-etl-bucket'
     file_name = 'compressed_log.gz'
     file_path = currentpath+'/logs/'+file_name
     
     #변수: 순서대로 로컬 파일명, s3의 버킷명, s3에 저장되는 파일명
     s3.upload_file(file_path, bucket_name, file_name)
+
+
+if __name__ == '__main__':
+   s3_upload()
